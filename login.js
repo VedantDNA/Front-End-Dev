c608e2f7d8089ff92e6e331d7d24049db507627f
VedantDNA <vedantdna@gmail.com>
2024-12-02 22:55:49 +0530

    Update login.js

--- login.js
+++ login.js
@@ -16,14 +16,12 @@ form.addEventListener("submit", (e) => {
     const email = emailInput.value;
     const password = passwordInput.value;
 
-    // Check if the user exists in localStorage
     const storedUser = localStorage.getItem(email);
 
     if (storedUser) {
         const user = JSON.parse(storedUser); 
 
         if (user.password === password) {
-            // Successful login
             localStorage.setItem('isLoggedIn', 'true');
             localStorage.setItem('loggedInUser', user.email);
             window.open('dashboard.html', '_self'); 
@@ -31,9 +29,7 @@ form.addEventListener("submit", (e) => {
             alert("Invalid password");
         }
     } else {
-        // Check against default user data
         if (defaultUser.username === email && defaultUser.password === password) {
-            // Successful login
             localStorage.setItem('isLoggedIn', 'true');
             localStorage.setItem('loggedInUser', defaultUser.username);
             window.open('dashboard.html', '_self'); 
